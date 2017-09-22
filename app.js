c7598c9e3f5417acc4b975bcacd62d5d12b9cf67
Kyle <ashcraft.k@husky.neu.edu>
2017-09-21 20:16:59 -0700

    added new app.js code structer

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 function onReady() {
-	/*const addToDoForm = document.getElementById('addToDoForm');
-	const newToDoText = document.getElementById('newToDoText');
-	const toDoList = document.getElementById('toDoList');
-	const formList = document.getElementById('list');
+	var toDos = [];
+	var addToDoForm = document.getElementById('addToDoForm');
 
-	addToDoForm.addEventListener('submit', (event) => {
+	function createNewToDo(){
+			var newToDoText = document.getElementById('newToDoText');
+			toDos.push({
+				title: newToDoText.value,
+				complete: false
+			});
+			newToDoText.value = '';
+
+			renderTheUI(toDos);
+	}
+
+	function renderTheUI(toDos){
+		var toDoList.getElementById('toDoList')
+
+		toDoList.innerHTML ='';
+
+		toDos.forEach(function(toDo){
+			var newLi = document.createElement('li')
+			var checkBox = document.createElement('input');
+			checkBox.type = 'checkbox';
+			newLi.innerHTML = toDo.title;
+
+			toDoList.appendChild(newLi);
+			newLi.appendChild(checkbox);
+		});
+	}
+
+	addToDoForm.addEventListener('submit', function(event) => {
 		event.preventDefault();
-		let title = newToDoText.value;
-		let newLi = document.createElement('li');
-		let checkbox = document.createElement('input');
-		checkbox.type = "checkbox";
-		checkbox.id="checkbox";
-		newLi.textContent = title;
-		toDoList.appendChild(newLi);
-		newLi.appendChild(checkbox);
-		newLi.id="todo";
-		newToDoText.value= '';
+		createNewToDo();
 	});
 
-	formList.addEventListener('submit', (event) => {
-		event.preventDefault();
-		let checkbox = document.getElementById("checkbox");
-		if (checkbox.checked){
-			checkbox.id="selected";
-			toDoList.removeChild(document.getElementById('todo') );
-		};
-	});
-*/
+
+
 
 }
 
